dd1a75559b72343a7da140f091208ccfb583e49d
pavan1815 <pavanreddyvancha@gmail.com>
2024-12-22 10:55:38 +0530

    deploy config3

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/React-Movie-App/",
+  base:"/React-Movie-Project/",
   build: {
     outDir: 'dist', // Ensure this matches your deployment command
 },
