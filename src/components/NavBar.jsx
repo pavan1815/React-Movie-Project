890d17e45b495543587cefa055cb58c7021edae8
pavan1815 <pavanreddyvancha@gmail.com>
2024-12-22 11:09:19 +0530

    info

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import "../css/Navbar.css"
 function NavBar(){
     return <nav className="navbar">
         <div className="navbar-brand">
-            <Link to="/">Movie App</Link>
+            <Link to="/">Movie App(Use VPN to open this page if movies doesn't load due to connected network)</Link>
         </div>
         <div className="navbar-links">
             <Link to="/React-Movie-Project/" className="nav-link">Home</Link>
